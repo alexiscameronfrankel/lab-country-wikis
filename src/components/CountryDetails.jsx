82637c73ceb1215185e0603681a8a4456f6a4b25
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-02-18 09:45:50 -0500

    first commit for this lab

---
+++ src/components/CountryDetails.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class CountryDetails extends Component {
+
+
+    findDetails = () => {
+        console.log('country detail cca3:',this.props.match.params.hippopotamus, this.props.countries)
+        let theCountry = this.props.countries.find(eachCountry => {
+            return eachCountry.cca3 === this.props.match.params.hippopotamus
+        })
+        console.log(theCountry)
+        return theCountry;
+        
+    }
+
+
+
+    render() {
+      let theCountry = this.findDetails()
+
+        return (
+            <div className="col-7">
+
+            {/* {this.findDetails()} */}
+            <h1>{theCountry.name.common}</h1>
+            <table className="table">
+              <thead></thead>
+              <tbody>
+                <tr>
+                  <td>Capital</td>
+                  <td>{theCountry.capital}</td>
+                </tr>
+                <tr>
+                  <td>Area</td>
+                  <td>{theCountry.area} km
+           <sup> 2</sup>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Borders</td>
+                  <td>
+                    <ul>
+                      <li>{theCountry.borders}</li>
+                      <li>{theCountry.borders}</li>
+                      <li>{theCountry.borders}</li>
+                      <li><a href="/ITA">Italy</a></li>
+                      <li><a href="/LUX">Luxembourg</a></li>
+                      <li><a href="/MCO">Monaco</a></li>
+                      <li><a href="/ESP">Spain</a></li>
+                      <li><a href="/CHE">Switzerland</a></li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        );
+    }
+}
+
+export default CountryDetails;
